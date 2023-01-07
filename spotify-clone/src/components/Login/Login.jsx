92cd76a41ae5cfdb293fde24b71eb7c84ec3b0a2
Alex <AlexGrimes.se@gmail.com>
2023-01-07 12:59:05 -0500

    getting token from spotify auth

--- spotify-clone/src/components/Login/Login.jsx
+++ spotify-clone/src/components/Login/Login.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./Login.css"
+import { loginUrl } from '../../Spotify/spotify'
 
 export default function Login() {
   return (
     <div className='login'>
 
         <img src='https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg' alt='Spotify logo'/>
-        <a href='#'>Login With Spotify</a>
+        <a href={loginUrl}>Login With Spotify</a>
     </div>
-  )
+  );
 }
