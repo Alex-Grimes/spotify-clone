c55d181cefb479128131c1a64a738a04246441f6
Alex <AlexGrimes.se@gmail.com>
2023-01-09 19:20:42 -0500

    install spotify and setup reducer

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Login from './components/Login/Login'
+import { getTokenFromUrl } from './Spotify/spotify'
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
 
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <div className="App">{token ? <h1>Logged in</h1>: <Login />}</div>
   )
 }
 
